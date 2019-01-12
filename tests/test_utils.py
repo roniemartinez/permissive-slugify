9a3dceeb0c6bfc0f595f25100ff2025c7d6f18a4
Ronie Martinez <ronmarti18@gmail.com>
2019-01-12 19:12:17 +0800

    Add test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,3 +12,9 @@ def test_smart_truncate_no_separator():
     txt = '1,000 reasons you are #1'
     r = smart_truncate(txt, max_length=100, separator='_')
     assert r == txt
+
+
+def test_smart_truncate_with_max_length():
+    txt = '1,000 reasons you are #1'*2
+    r = smart_truncate(txt, max_length=20, separator='_', word_boundary=True)
+    assert r == (txt*10)[:20]
